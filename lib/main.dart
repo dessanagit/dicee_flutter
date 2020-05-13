e116b2e4b8d035e3c21dc51163545313b904d6c8
dessanagit <dessanacms@gmail.com>
2020-05-13 19:05:49 -0400

    Apply Stateful Widget

--- lib/main.dart
+++ lib/main.dart
@@ -15,25 +15,44 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+// Stateful Widget -> stful
+// It allows changes in the app's state. So, the state of Stateless Widget is not meant to change.
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  // a variable set place above @override in order to be created once.
+  // if inside the build method, it is going to be rebuilding every time on hotreload, etc.
+//  CONCLUSION: HERE YOU CREATE THE VARIABLE
+  int leftDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
+// HERE YOU UPDATE THE VARIABLE (changing the State)
     return Center(
       child: Row(
         children: <Widget>[
           // Expanded() creates flexible layouts
+          // void callback -> () { //do something }
+          // function in Dart -> void Getmilk() {}
           Expanded(
             child: FlatButton(
               onPressed: () {
-                print('Left button got pressed');
+                setState(() {
+                  leftDiceNumber = 5;
+                  print('Left button got pressed');
+                });
               },
-              child: Image.asset("images/dice1.png"),
+              // String Interpolation -> To insert a variable inside a string uses dollar sign ($).
+              child: Image.asset("images/dice$leftDiceNumber.png"),
             ),
           ),
           Expanded(
             child: FlatButton(
               // Arrow function
-              onPressed: () => print('Right button got pressed.'),
+              onPressed: () =>
+                  print('Right button got pressed.'), // void callback
               child: Image.asset("images/dice1.png"),
             ),
           ),
