7b4dafc29c80c5f0004a4e6db3a7b77d05eae9ae
dessanagit <dessanacms@gmail.com>
2020-05-13 20:30:22 -0400

    Complete Project

--- lib/main.dart
+++ lib/main.dart
@@ -28,11 +28,21 @@ class _DicePageState extends State<DicePage> {
   // a variable set place above @override in order to be created once.
   // if inside the build method, it is going to be rebuilding every time on hotreload, etc.
 //  CONCLUSION: HERE YOU CREATE THE VARIABLE
+//https://stackoverflow.com/questions/59710334/placing-int-inside-built-function-dont-change-image-of-dice-but-when-move-it
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
+
+  void changeDiceNumber() {
+    // nextInt() method Generates a non-negative random integer uniformly distributed in the range from 0 (inclusive) to max (exclusive).
+    // So it is necessary to add +1 to achieve the range between 1 and 6.
+    leftDiceNumber = Random().nextInt(6) + 1; // 0 - 5 => 1 - 6
+    rightDiceNumber = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
 // HERE YOU UPDATE THE VARIABLE (changing the State)
+
     return Center(
       child: Row(
         children: <Widget>[
@@ -44,9 +54,7 @@ class _DicePageState extends State<DicePage> {
               onPressed: () {
                 setState(
                   () {
-                    // nextInt() method Generates a non-negative random integer uniformly distributed in the range from 0 (inclusive) to max (exclusive).
-                    // So it is necessary to add +1 to achieve the range between 1 and 6.
-                    leftDiceNumber = Random().nextInt(6) + 1; // 0 - 5 => 1 - 6
+                    changeDiceNumber();
                     print('Left button got pressed');
                   },
                 );
@@ -60,7 +68,7 @@ class _DicePageState extends State<DicePage> {
               // Arrow function
               onPressed: () {
                 setState(() {
-                  rightDiceNumber = Random().nextInt(6) + 1;
+                  changeDiceNumber();
                   print('Right button got pressed.');
                 });
               }, // void callback
